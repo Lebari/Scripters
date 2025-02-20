b877235b8ad3524a9710de7c337496f575cb25d9
Greatlove Bariboloka <greatlebari@gmail.com>
2025-02-20 02:48:16 -0500

    Started UC7-8 - auction upload and dutch auction update

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,41 @@
 # Schema for mapping Mongo Document structures to Python classes
+from mongoengine import Document, StringField, IntField, ListField, ReferenceField
+
+
+class Item(Document):
+    name = StringField(required=True)
+    price = IntField(required=True)
+    status = StringField(required=True)
+    category = StringField(required=True)
+
+
+class User(Document):
+    fname = StringField(required=True)
+    lname = StringField(required=True)
+    username = StringField(required=True)
+    password = StringField(required=True)
+
+
+class Seller(User):
+    sales = ListField() #TODO change when Sale class is created
+    auctions = ListField(Auction) #TODO ensure this works
+
+
+class Auction(Document):
+    name = StringField(required=True)
+    duration = IntField(required=True)
+    item = Item(required=True)
+    type = StringField(required=True)
+    seller = ReferenceField(Seller, required=True)
+
+    #TODO fix below by creating needed classes
+    event = Event()
+    broker = Broker()
+    bids = ListField(Bid)
+
+    def json_formatted(self):
+        print(f"serializing {self.__str__}")
+        model_json = self.to_mongo().to_dict()
+        model_json["id"] = str(model_json["_id"])
+        del model_json["_id"]
+        return model_json
--- backend/app/src/catalog/routes.py
+++ backend/app/src/catalog/routes.py
@@ -1,8 +1,63 @@
 from . import catalog
-from flask import jsonify
+from ...models import Auction, Item
+from flask import jsonify, request
 
 
 @catalog.route("/", methods=["GET"])
 def hello_catalog():
     print("Hello from Catalog")
     return jsonify({"message": "Hello from Catalog"}), 201
+
+
+@catalog.route("/upload", methods=["POST"])
+# TODO add seller authorization check
+def upload_auction():
+    print("Hello from upload_auction")
+
+    try:
+        # get arguments from json
+        data = request.json
+
+        # TODO add more args validations
+        # create item
+        new_item = Item(
+            name = data["item_name"],
+            price = data["item_price"],
+            status = data["item_status"],
+            category = data["item_category"]
+        )
+        new_item.save()
+
+        #create auction
+        new_auction = Auction(
+            name = data["name"],
+            duration = data["duration"],
+            item = new_item,
+            type = data["type"],
+            seller = data["seller"]
+        )
+        new_auction.save()
+
+        return jsonify({"message": "New auction uploaded"}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+
+@catalog.route("/dutch-update", methods=["PUT"])
+# TODO add seller authorization check
+def update_dutch_auction():
+    print("Hello from update_dutch_auction")
+
+    try:
+        # get price from json
+        new_price = request.json["price"] # TODO add more args validations
+        auction_id = request.json["auction_id"]
+
+        # Retrieve auction from database
+        auction = Auction.objects(id=auction_id).first()
+        auction.price = new_price
+        auction.save()
+
+        return jsonify({"message": "Auction price updated"}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
