51c67522f167b554237de76bd4f760a0d40152ae
Abdullah-Sawal <90650822+Abdullah-Sawal@users.noreply.github.com>
2025-02-28 19:17:26 -0500

    Completed model implementations in model.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -11,35 +11,66 @@ class Item(Document):
 
 
 class User(Document):
+    meta={'allow_inheritance': True}
     fname = StringField(required=True)
     lname = StringField(required=True)
     username = StringField(required=True)
     password = StringField(required=True)
 
+class Event(Document):
+    meta={'allow_inheritance': True}
+    user = ReferenceField(User, required=True)
+    type = StringField(required=True)
+    time = DateTimeField(required=True)
+    price = IntField(required=True)
+    auction = ReferenceField('Auction', required=True)
+
+
+class Bid(Event):
+    pass
+
+
+class Sale(Event):
+    card = ReferenceField('Card', required=True)
+
+class Broker(Document):
+    subscriptions = ListField(StringField())
+
 
 class Seller(User):
-    sales = ListField(Sale) #TODO 1 create Sale class
-    auctions = ListField(Auction) #TODO ensure this works
+    sales = ListField(ReferenceField(Sale)) 
+    auctions = ListField(ReferenceField('Auction')) 
 
 
 class Auction(Document):
-    item = Item(required=True)
+    item = ReferenceField(Item, required=True)
     slug = StringField(required=True)
     type = StringField(required=True)
     duration = IntField(required=True)
     seller = ReferenceField(Seller, required=True)
 
-    date_added: DateTimeField()
-    date_updated: DateTimeField()
+    date_added = DateTimeField()
+    date_updated = DateTimeField()
 
     #TODO 2 fix below by creating needed classes
-    event = Event()
-    broker = Broker()
-    bids = ListField(Bid)
+    event = ReferenceField(Event)
+    broker = ReferenceField(Broker)
+    bids = ListField(ReferenceField(Bid))
 
     def json_formatted(self):
-        print(f"serializing {self.__str__}")
+        print(f"serializing {self.str}")
         model_json = self.to_mongo().to_dict()
         model_json["id"] = str(model_json["_id"])
         del model_json["_id"]
-        return model_json
+        return model_json 
+
+class Card(Document):
+    name = StringField(required=True)
+    number = IntField(required=True, unique=True)
+    cvv = IntField(required=True)
+    expiry = DateTimeField(required=True)
+
+
+
+
+
